c491c28cf53b1dd230c33b0f8f31713f83b9a3a4
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2025-02-03 00:01:14 +0630

    first

---
+++ app/lib/store.ts
@@ -0,0 +1,12 @@
+/*import {create} from "zustand"
+
+type CounterStore = {
+    count: number;
+} 
+
+export const useCounterStore = create<CounterStore>(()=>({
+    count: 0,
+}));
+
+//
+const count = useCounterStore((state)=>state.count);*/
